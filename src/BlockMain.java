e8cd4731e4bc4fca04536bcf7f5ad808ebcbf7d5
Parneet kaur Jattana <Parneet kaur Jattana@192.168.0.22>
2019-08-09 06:36:56 -0400

    Phase4 committ

--- src/BlockMain.java
+++ src/BlockMain.java
@@ -1,15 +1,18 @@
+import java.util.ArrayList;
 
 public class BlockMain {
-
+	public static ArrayList<Block_Chain> blockchain = new ArrayList<Block_Chain>(); 
 	public static void main(String[] args) {
-
-		Block_Chain MyGenesisBlock=new Block_Chain("ThisIsMyGenesisBlock");
-		System.out.println("This is my Genesis block: " +MyGenesisBlock.CalculateHash());
+	
 		
-		//data is changed and hash also changes when data changes
-		MyGenesisBlock.setData("ThisIsMyChangedData");
-		System.out.println("This is my changed hash code: "+MyGenesisBlock.CalculateHash());
-
+			//add our blocks to the blockchain ArrayList:
+			blockchain.add(new Block_Chain("Hi im the first block", "0"));		
+			blockchain.add(new Block_Chain("Yo im the second block",blockchain.get(blockchain.size()-1).getHash())); 
+			blockchain.add(new Block_Chain("Hey im the third block",blockchain.get(blockchain.size()-1).getHash()));
+			
+		}
+		
+		
 	}
 
-}
+
--- src/Block_Chain.java
+++ src/Block_Chain.java
@@ -3,13 +3,25 @@ public class Block_Chain
 {
 	private String hash;
 	private String data;
+	private String previousHash;
 	
 public Block_Chain(String data)
 {
 	this.data=data;
 	this.hash=hash;
 }
+public Block_Chain(String data,String hash)
+{
+	this.data=data;
+	this.previousHash=hash;
+}
 
+public String getPreviousHash() {
+	return previousHash;
+}
+public void setPreviousHash(String previousHash) {
+	this.previousHash = previousHash;
+}
 public String getHash() {
 	return hash;
 }
